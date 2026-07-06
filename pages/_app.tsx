495b041dd2e1a0b2c04384139c8a0115ef91c676
Reddy <info@buildmitra.com>
2026-07-06 16:58:41 +0530

    Fix PaymentBarrierProvider app wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import Sidebar from "../components/Sidebar";
 import { useRouter } from "next/router";
 import ClientErrorBoundary from "../components/ClientErrorBoundary";
+import { PaymentBarrierProvider } from "../hooks/usePaymentBarrier";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
@@ -11,13 +12,15 @@ export default function App({ Component, pageProps }) {
 
   return (
     <ClientErrorBoundary>
-      {showSidebar ? (
-        <Sidebar currentPath={router.pathname}>
+      <PaymentBarrierProvider>
+        {showSidebar ? (
+          <Sidebar currentPath={router.pathname}>
+            <Component {...pageProps} />
+          </Sidebar>
+        ) : (
           <Component {...pageProps} />
-        </Sidebar>
-      ) : (
-        <Component {...pageProps} />
-      )}
+        )}
+      </PaymentBarrierProvider>
     </ClientErrorBoundary>
   );
 }
